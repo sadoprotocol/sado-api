edb7911e0807645d842464c9753ad733ea8b8e38
Kodemon <hi@kodemon.net>
2023-04-27 16:11:49 +0900

    chore(ts): add temporary performance metrics

--- src/orderbook/orderbook.ts
+++ src/orderbook/orderbook.ts
@@ -16,6 +16,8 @@ export class OrderBook {
   readonly orders: Orders;
   readonly offers: Offers;
 
+  readonly #ts: number[] = [];
+
   constructor(readonly address: string, readonly options: Options) {
     this.orders = new Orders(options.network);
     this.offers = new Offers(options.network);
@@ -28,10 +30,14 @@ export class OrderBook {
   async resolve(): Promise<this> {
     log(`${this.network}: Resolving Orderbook`);
 
+    const t = performance.now();
+
     const txs = await lookup.transactions(this.address, this.network);
     if (txs.length === 0) {
       return this;
     }
+
+    this.#ts.push(performance.now() - t);
 
     log(`${this.network}: Found ${txs.length} transactions`);
 
@@ -48,6 +54,7 @@ export class OrderBook {
    * @param txs - Transactions to process.
    */
   async #process(txs: Transaction[]): Promise<void> {
+    const t = performance.now();
     for (const tx of txs) {
       for (const vout of tx.vout) {
         const sado = parseSado(vout.scriptPubKey.utf8);
@@ -60,6 +67,7 @@ export class OrderBook {
         }
       }
     }
+    this.#ts.push(performance.now() - t);
   }
 
   /**
@@ -67,11 +75,14 @@ export class OrderBook {
    * a format improves referencing relations in clients.
    */
   async #link() {
+    const t = performance.now();
     this.orders.linkOffers(this.offers);
+    this.#ts.push(performance.now() - t);
   }
 
   toJSON() {
     return {
+      ts: this.#ts.map((t) => roundToNearestNonZeroDecimal(t / 1_000)),
       analytics: {
         orders: this.orders.analytics,
         offers: this.offers.analytics,
@@ -115,6 +126,25 @@ function parseSado(utf8?: string): SadoOrder | undefined {
       return { type, cid };
     }
   }
+}
+
+function roundToNearestNonZeroDecimal(num: number): number {
+  let decimalPlace = 0;
+  let value = num;
+
+  if (value > 0) {
+    while (value < 1) {
+      value *= 10;
+      decimalPlace++;
+    }
+  } else if (value < 0) {
+    while (value > -1) {
+      value *= 10;
+      decimalPlace++;
+    }
+  }
+
+  return parseFloat(num.toFixed(decimalPlace));
 }
 
 /*
