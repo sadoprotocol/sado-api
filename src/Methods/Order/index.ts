93bb581b3e33bb30a5962a283449f0c32193a7a5
Kodemon <hi@kodemon.net>
2023-07-03 15:45:28 +0900

    chore(methods): remove unused method

--- src/Methods/Order/index.ts
+++ src/Methods/Order/index.ts
@@ -1,10 +1,8 @@
 import { api } from "../../Api";
-import { createInstantBuyPsbt } from "./CreateInstantBuyPsbt";
 import { createOrder } from "./CreateOrder";
 import { createSignablePsbt } from "./CreateSignablePsbt";
 import { getOrder } from "./GetOrder";
 
 api.register("CreateOrder", createOrder);
-api.register("CreateInstantBuyPsbt", createInstantBuyPsbt);
 api.register("CreateSignablePsbt", createSignablePsbt);
 api.register("GetOrder", getOrder);
