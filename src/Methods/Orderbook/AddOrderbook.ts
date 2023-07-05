07c0c9915e6387555ea1f75e843e103b26fff6eb
Kodemon <hi@kodemon.net>
2023-07-05 15:11:50 +0900

    chore(orderbooks): add orderbook method

---
+++ src/Methods/Orderbook/AddOrderbook.ts
@@ -0,0 +1,18 @@
+import Schema, { string } from "computed-types";
+
+import { method } from "../../Libraries/JsonRpc/Method";
+import { DEFAULT_NETWORK } from "../../Libraries/Network";
+import { hasValidToken } from "../../Middleware/HasValidToken";
+import { resolveOrderbook } from "../../Orderbook/Resolver";
+import { validate } from "../../Validators";
+
+export const addOrderbook = method({
+  params: Schema({
+    address: string,
+    network: validate.schema.network.optional(),
+  }),
+  actions: [hasValidToken],
+  handler: async ({ address, network = DEFAULT_NETWORK }): Promise<void> => {
+    await resolveOrderbook(address, network);
+  },
+});
--- src/Methods/Orderbook/index.ts
+++ src/Methods/Orderbook/index.ts
@@ -1,10 +1,12 @@
 import { api } from "../../Api";
+import { addOrderbook } from "./AddOrderbook";
 import { getOrderbookAnalytics } from "./Analytics";
 import { getOrderbook } from "./GetOrderbook";
 import { getOrderbookOffers } from "./Offers";
 import { getOrderbookOrders } from "./Orders";
 import { rebuildOrderbook } from "./RebuildOrderbook";
 
+api.register("AddOrderbook", addOrderbook);
 api.register("GetOrderbookAnalytics", getOrderbookAnalytics);
 api.register("GetOrderbook", getOrderbook);
 api.register("GetOrderbookOrders", getOrderbookOrders);
